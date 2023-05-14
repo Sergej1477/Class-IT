f82c60abc835928302e68cecadd47d9bda76d18a
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-05-14 16:28:59 +0300

    added filles2 less-12

--- lession-12/index.js
+++ lession-12/index.js
@@ -1,1 +1,37 @@
-console.log('asasasasa')
+// function renderSymbol(symbol, count){
+// for (let sym = symbol; sym.length < count; sym += symbol){
+//     console.log(sym)
+// }
+// }
+// renderSymbol('@_@', 20)
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+// function isPolindrom(word) {
+//   let temp = "";
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     temp += word[i];
+//   }
+//   return word === temp;
+// }
+
+// console.log(isPolindrom("кабак"));
+
+// function countLetters(word, letter) {
+//   let temp = 0;
+//   for (let i = 0; i <= word.length; i++)
+//     if (word[i] === letter) {
+//       temp += 1;
+//     }
+//   return temp;
+// }
+
+// console.log(countLetters("Hellllooo", "o"));
+
+
