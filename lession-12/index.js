e81a7d8ffb77835c02c98673eb45d93365b91ebb
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-05-15 10:20:47 +0300

    added files3 less-12

--- lession-12/index.js
+++ lession-12/index.js
@@ -34,4 +34,54 @@
 
 // console.log(countLetters("Hellllooo", "o"));
 
+// const str = 'stringer'
+// const temp = str.slice(2, 4)
+
+// console.log(temp)
+
+// const str = 'stringer';
+// const temp = str.substr(1, 5);
+
+// console.log(temp)
+
+// const str = 'aaa@bbb@ccc'
+// const temp = str.replaceAll('@', '!')
+
+// console.log(temp)
+
+// const str = 'hello'
+// const temp = str.slice(1)
+// const temp2 = 'H'
+// const temp3 = temp2.concat(temp)
+
+// console.log(temp3)
+
+// const str = 'aaa bbb ccc'
+// const temp = str.substr(4, 3 )
+// const temp2 = str.substring(4, 7)
+// const temp3 = str.slice(4, 7)
+
+// console.log(temp,temp2,temp3)
+
+// function forEach(arr, callback) {
+//     for (let index = 0; index < arr.length; index++){
+//         callback(index[index], index, arr);
+//     }
+// };
+
+// forEach([1,2,3,4,5], callback)
+
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     console.log(true);
+//   } else (
+//     console.log(false));
+// }
+// isEven(8);
+
+// function isEven(number) {
+//   return number % 2 === 0;
+// }
+// isEven(8);
+
 
